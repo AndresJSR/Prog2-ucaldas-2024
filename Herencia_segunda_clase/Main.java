06678498a25a77a0f6939a053f76427c6cc33463
AndresJSR <andres.sanchez45351@ucaldas.edu.co>
2024-04-21 17:11:06 -0500

    Ejercicio de herencia

--- Herencia_segunda_clase/Main.java
+++ Herencia_segunda_clase/Main.java
@@ -6,34 +6,7 @@ public class Main{
         
         
         instance_student.leerArchivosTxt();
-        instance_student.Menu();
+        instance_student.IsEligibleToEnroll();
     }
-    public void menu(){
-    int opcion=0;
-    do{
-        System.out.println("1 listar materias totales o inscribir materias");
-        System.out.println("2 listar  materias inscritas");
-        System.out.println("3 salir");
-        opcion = sc.nextInt();
-        sc.nextLine();
-
-        switch (opcion) {
-            case 1:
-                instance_student.IsEligibleToEnroll();
-                break;
-        
-            case 2:
-                instance_student.GetSeminarsTaken();
-                break;
-            
-            case 3:
-                break;
-
-            default:
-                System.out.println("Opción Incorrecta");
-                break;
-        }
-
-    }while(opcion!=3);
-}
+    
 }
